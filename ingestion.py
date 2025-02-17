5a4dcbba485f3e497f303a5d24863a494b923685
huy.to.ryan <huy.to.ryan@gmail.com>
2025-02-17 17:36:31 +0700

    Change ingestion

--- ingestion.py
+++ ingestion.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
+from langchain_pinecone import PineconeVectorStore
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
+from langchain_ollama import OllamaEmbeddings
 
 load_dotenv()
 
@@ -20,15 +22,23 @@ text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
 )
 doc_splits = text_splitter.split_documents(docs_list)
 
-# vectorstore = Chroma.from_documents(
-#     documents=doc_splits,
-#     collection_name="rag-chroma",
-#     embedding=OpenAIEmbeddings(),
-#     persist_directory="./.chroma",
-# )
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    # embedding=OpenAIEmbeddings(),
+    embbeding_function=OllamaEmbeddings(model="llama3.1:70b"),
+    persist_directory="./.chroma",
+)
 
 retriever = Chroma(
     collection_name="rag-chroma",
     persist_directory="./.chroma",
-    embedding_function=OpenAIEmbeddings(),
+    # embedding_function=OpenAIEmbeddings(),
+    embbeding_function=OllamaEmbeddings(model="llama3.1:70b"),
 ).as_retriever()
+
+retriever_anime = PineconeVectorStore(
+    index_name='firecrawl-index', 
+    embedding=OllamaEmbeddings(model="llama3.1:70b")
+).as_retriever()
+
